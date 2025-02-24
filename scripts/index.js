454a2904b81b22e7dd0a14651a8d6bc5837bd796
Nastya Olennikova <olennikovanastyaa@yandex.ru>
2025-02-24 12:06:00 +0100

    added functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(name, link) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCard(initialCards) {
+  initialCards.forEach((element) => {
+    const cardElement = createCard(element.name, element.link);
+    placesList.appendChild(cardElement);
+  });
+}
+
+renderCard(initialCards);
